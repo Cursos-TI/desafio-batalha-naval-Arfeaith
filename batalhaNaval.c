a5da678e9fd9c3cdbe4b6a1aab0214c4200a7c52
Vitor Pereira <vitordspereira00@gmail.com>
2025-08-19 15:08:26 -0300

    Nível Mestre v1.0

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,7 +3,16 @@
 //Definir constantes para fácil acesso
 #define TAM_NAVIO 3
 #define TAM_TABULEIRO 10
+#define TAM_HABILIDADES_L 3
+#define TAM_HABILIDADES_C 5
+#define PONTO_ORIGEM_CONE_L 7
+#define PONTO_ORIGEM_CONE_C 0
+#define PONTO_ORIGEM_CRUZ_L 3
+#define PONTO_ORIGEM_CRUZ_C 5
+#define PONTO_ORIGEM_OCTA_L 3
+#define PONTO_ORIGEM_OCTA_C 0
 #define REPRE_NAVIO 3
+#define REPRE_HABILIDADES 5
 #define AGUA 0
 
 int main() {
@@ -14,10 +23,14 @@ int main() {
     int navioV [TAM_NAVIO];
     int navioD1 [TAM_NAVIO];
     int navioD2 [TAM_NAVIO];
+    int cone [TAM_HABILIDADES_L] [TAM_HABILIDADES_C];
+    int cruz [TAM_HABILIDADES_L] [TAM_HABILIDADES_C];
+    int octaedro [TAM_HABILIDADES_L] [TAM_HABILIDADES_C];
+    int centro = TAM_HABILIDADES_C / 2;
     char linha [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
     int linha_navioH = 2, coluna_navioH = 4;
-    int linha_navioV = 5, coluna_navioV = 6;
+    int linha_navioV = 7, coluna_navioV = 9;
     int linha_navioD1 = 0, coluna_navioD1 = 0;
     int linha_navioD2 = 0, coluna_navioD2 = 9;
 
@@ -40,8 +53,105 @@ int main() {
         navioD2[i] = REPRE_NAVIO;
     }
     
-    //Checar se os navios cabem no tabuleiro e os posicionando
+    //Colocar representação da água nas matrizes das habilidades
+    for (int i = 0; i < TAM_HABILIDADES_L; i++)
+    {
+        for (int j = 0; j < TAM_HABILIDADES_C; j++)
+        {
+            cone [i][j] = AGUA;
+            cruz [i][j] = AGUA;
+            octaedro [i][j] = AGUA;
+        }
+        
+    }
 
+    //Substituir água para representação das habilidades
+    for (int i = 0; i < TAM_HABILIDADES_L; i++)
+    {
+        for (int j = 0; j < TAM_HABILIDADES_C; j++)
+        {
+            if (j >= centro - i && j <= centro + i)
+            {
+                cone [i][j] = REPRE_HABILIDADES;
+            }
+            
+        }
+    }
+
+    for (int i = 0; i < TAM_HABILIDADES_L; i++)
+    {
+        for (int j = 0; j < TAM_HABILIDADES_C; j++)
+        {
+            if (i == TAM_HABILIDADES_L / 2 || j == TAM_HABILIDADES_C / 2)
+            {
+                cruz [i][j] = REPRE_HABILIDADES;
+                
+            }
+            
+        }
+        
+    }
+
+    for (int i = 0; i < TAM_HABILIDADES_L; i++)
+    {
+        for (int j = 0; j < TAM_HABILIDADES_C; j++)
+        {
+            int distanciaL = (i > TAM_HABILIDADES_L / 2) ? i - (TAM_HABILIDADES_L / 2) : (TAM_HABILIDADES_L / 2) - i;
+            int distanciaC = (j > TAM_HABILIDADES_C / 2) ? j - (TAM_HABILIDADES_C / 2) : (TAM_HABILIDADES_C / 2) - j;
+
+            if (distanciaL + distanciaC <= TAM_HABILIDADES_L / 2)
+            {
+                octaedro [i][j] = REPRE_HABILIDADES;
+            }
+            
+        }
+        
+    }
+    
+    //Checar se as habilidades cabem no tabuleiro e coloca-las no tabuleiro
+    if (0 <= PONTO_ORIGEM_CRUZ_L && PONTO_ORIGEM_CRUZ_L <= 7 && 0 <= PONTO_ORIGEM_CRUZ_C && PONTO_ORIGEM_CRUZ_C <= 5)
+    {
+        for (int i = 0; i < TAM_HABILIDADES_L; i++)
+        {
+            for (int j = 0; j < TAM_HABILIDADES_C; j++)
+            {
+            tabuleiro [PONTO_ORIGEM_CRUZ_L + i][PONTO_ORIGEM_CRUZ_C + j] = cruz [i][j];
+            }
+        }
+    } else {
+        printf("A cruz não cabe no tabuleiro.");
+        return 0;
+    }
+
+    if (0 <= PONTO_ORIGEM_CONE_L && PONTO_ORIGEM_CONE_L <= 7 && 0 <= PONTO_ORIGEM_CONE_C && PONTO_ORIGEM_CONE_C <= 5 )
+    {
+        for (int i = 0; i < TAM_HABILIDADES_L; i++)
+        {
+            for (int j = 0; j < TAM_HABILIDADES_C; j++)
+            {
+                tabuleiro [PONTO_ORIGEM_CONE_L + i][PONTO_ORIGEM_CONE_C + j] = cone [i][j];
+            }
+        }
+    } else {
+        printf("O cone não cabe no tabuleiro.");
+        return 0;
+    }
+
+    if (0 <= PONTO_ORIGEM_OCTA_L && PONTO_ORIGEM_OCTA_L <= 7 && 0 <= PONTO_ORIGEM_OCTA_C && PONTO_ORIGEM_OCTA_C <= 5 )
+    {
+        for (int i = 0; i < TAM_HABILIDADES_L; i++)
+        {
+            for (int j = 0; j < TAM_HABILIDADES_C; j++)
+            {
+                tabuleiro [PONTO_ORIGEM_OCTA_L + i][PONTO_ORIGEM_OCTA_C + j] = octaedro [i][j];
+            }
+        }
+    } else {
+        printf("O octaedro não cabe no tabuleiro.");
+        return 0;
+    }
+
+    //Checar se os navios cabem no tabuleiro e os posicionar
     if (coluna_navioD1 + TAM_NAVIO <= TAM_TABULEIRO || linha_navioD1 + TAM_NAVIO <= TAM_TABULEIRO)
     {
         int sobrepoe = 0;
@@ -153,6 +263,8 @@ int main() {
         printf("Navio na vertical não cabe no tabuleiro.\n");
         return 0;
     }
+    
+    
 
     //Exibir tabuleiro
     printf("\n   ");
